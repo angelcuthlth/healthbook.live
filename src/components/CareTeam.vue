eaefc4e047aed81ed8b8093026021cc674cfb9f7
Jermaine Watkins <2@weathercheck.co>
2019-10-27 07:08:03 -0700

    wip

--- src/components/CareTeam.vue
+++ src/components/CareTeam.vue
@@ -1,64 +1,12 @@
 <template>
   <div class="theme-layout">
     <section>
-      <div class="feature-photo">
-        <figure><img src="../images/resources/timeline-1.jpg" alt=""></figure>
-        <router-link :to="{name: 'faq'}">
-          <button id="myButton" class="btn">Continue</button>
-        </router-link>
-        <form class="edit-phto">
-          <i class="fa fa-camera-retro"></i>
-          <label class="fileContainer">
-            Edit Cover Photo
-            <input type="file"/>
-          </label>
-        </form>
-        <div class="container-fluid">
-          <div class="row merged">
-            <div class="col-lg-2 col-sm-3">
-              <div class="user-avatar">
-                <figure>
-                  <img src="../images/resources/user-avatar.jpg" alt="">
-                  <form class="edit-phto">
-                    <i class="fa fa-camera-retro"></i>
-                    <label class="fileContainer">
-                      Edit Display Photo
-                      <input type="file"/>
-                    </label>
-                  </form>
-                </figure>
-              </div>
-            </div>
-            <div class="col-lg-10 col-sm-9">
-              <div class="timeline-info">
-                <ul>
-                  <li class="admin-name">
-                    <h5>Janice Griffith</h5>
-                    <span>Group Admin</span>
-                  </li>
-                  <li>
-                    <a class="" href="#" title="" data-ripple="">time line</a>
-                    <a class="" href="#" title="" data-ripple="">Photos</a>
-                    <a class="" href="#" title="" data-ripple="">Videos</a>
-                    <a class="" href="#" title="" data-ripple="">Friends</a>
-                    <a class="" href="#" title="" data-ripple="">Groups</a>
-                    <a class="" href="#" title="" data-ripple="">about</a>
-                    <a class="active" href="#" title="" data-ripple="">more</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section><!-- top area -->
-    <section>
       <div class="gap gray-bg">
         <div class="container">
           <div class="row">
             <div class="col-lg-12">
               <div class="row merged20" id="page-contents">
-                <div class="col-lg-9">
+                <div class="col-lg-12">
                   <div class="inbox-sec">
                     <div class="row">
                       <div class="col-lg-3 col-md-3 col-sm-4">
@@ -68,6 +16,9 @@
                             <h1><i>Brian</i> Kelly</h1>
                             <a title="" href="#"><i class="fa fa-user"></i>Profile</a>
                             <a title="" href="#"><i class="fa fa-cog"></i>Setting</a>
+                            <router-link :to="{name: 'faq'}">
+                              <button id="myButton" class="btn">Continue</button>
+                            </router-link>
                             <ul>
                               <li><a class="compose-btn" title="" href="#">Compose</a></li>
                             </ul>
@@ -225,82 +176,6 @@
                     </div>
                   </div>
                 </div>
-                <div class="col-lg-3">
-                  <aside class="sidebar static">
-                    <div class="advertisment-box">
-                      <h4 class="">advertisment</h4>
-                      <figure>
-                        <a href="#" title="Advertisment"><img src="../images/resources/ad-widget.gif" alt=""></a>
-                      </figure>
-                    </div>
-                    <div class="widget friend-list stick-widget">
-                      <h4 class="widget-title">Friends</h4>
-                      <div id="searchDir"></div>
-                      <div class="chat-box">
-                        <div class="chat-head">
-                          <span class="status f-online"></span>
-                          <h6>Bucky Barnes</h6>
-                          <div class="more">
-                            <span><i class="ti-more-alt"></i></span>
-                            <span class="close-mesage"><i class="ti-close"></i></span>
-                          </div>
-                        </div>
-                        <div class="chat-list">
-                          <ul>
-                            <li class="me">
-                              <div class="chat-thumb"><img src="../images/resources/chatlist1.jpg" alt=""></div>
-                              <div class="notification-event">
-															<span class="chat-message-item">
-																Hi James! Please remember to buy the food for tomorrow! I’m gonna be handling the gifts and Jake’s gonna get the drinks
-															</span>
-                                <span class="notification-date"><time datetime="2004-07-24T18:18" class="entry-date updated">Yesterday at 8:10pm</time></span>
-                              </div>
-                            </li>
-                            <li class="you">
-                              <div class="chat-thumb"><img src="../images/resources/chatlist2.jpg" alt=""></div>
-                              <div class="notification-event">
-															<span class="chat-message-item">
-																Hi James! Please remember to buy the food for tomorrow! I’m gonna be handling the gifts and Jake’s gonna get the drinks
-															</span>
-                                <span class="notification-date"><time datetime="2004-07-24T18:18" class="entry-date updated">Yesterday at 8:10pm</time></span>
-                              </div>
-                            </li>
-                            <li class="me">
-                              <div class="chat-thumb"><img src="../images/resources/chatlist1.jpg" alt=""></div>
-                              <div class="notification-event">
-															<span class="chat-message-item">
-																Hi James! Please remember to buy the food for tomorrow! I’m gonna be handling the gifts and Jake’s gonna get the drinks
-															</span>
-                                <span class="notification-date"><time datetime="2004-07-24T18:18" class="entry-date updated">Yesterday at 8:10pm</time></span>
-                              </div>
-                            </li>
-                          </ul>
-                          <form class="text-box">
-                            <textarea placeholder="Post enter to post..."></textarea>
-                            <div class="add-smiles">
-                              <span title="add icon" class="em em-expressionless"></span>
-                            </div>
-                            <div class="smiles-bunch">
-                              <i class="em em---1"></i>
-                              <i class="em em-smiley"></i>
-                              <i class="em em-anguished"></i>
-                              <i class="em em-laughing"></i>
-                              <i class="em em-angry"></i>
-                              <i class="em em-astonished"></i>
-                              <i class="em em-blush"></i>
-                              <i class="em em-disappointed"></i>
-                              <i class="em em-worried"></i>
-                              <i class="em em-kissing_heart"></i>
-                              <i class="em em-rage"></i>
-                              <i class="em em-stuck_out_tongue"></i>
-                            </div>
-                            <button type="submit"></button>
-                          </form>
-                        </div>
-                      </div>
-                    </div><!-- friends list sidebar -->
-                  </aside>
-                </div><!-- sidebar -->
               </div>
             </div>
           </div>
