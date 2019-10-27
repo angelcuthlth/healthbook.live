c3feb79ddcf1cdb60ab5d4ea5fac529bd85f4761
aseals <seals.pro@gmail.com>
2019-10-27 12:38:47 -0700

    update

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -8,74 +8,292 @@
               <div class="col-lg-12">
                 <div class="row merged20" id="page-contents">
                   <div class="col-lg-3">
+                    <div class="align-left">
+                        <a href="#" title=""><img src="../images/resources/health-logo.png" style="
+                                                                                                padding-bottom: 25px;
+                                                                                            "></a>
+                    </div>
                     <div class="sidebar static">
                       <div class="widget">
                         <h4 class="widget-title">Shortcuts</h4>
                         <ul class="naves">
                           <li>
                             <i class="ti-clipboard"></i>
-                            <a href="#" title="">News feed</a>
+                            <a href="#" title="">Feed</a>
                           </li>
+                          <li>
+                          <i class="ti-mouse-alt"></i>
+                          <a href="#" title="">Explore Data</a>
+                            </li>
                           <li>
                             <i class="ti-mouse-alt"></i>
                             <a href="#" title="">Inbox</a>
-                          </li>
-                          <li>
-                            <i class="ti-files"></i>
-                            <a href="#" title="">My pages</a>
                           </li>
                         </ul>
                       </div><!-- Shortcuts -->
                     </div>
                   </div><!-- sidebar -->
                   <div class="col-lg-6">
-                    <div class="central-meta">
-                      <router-link :to="{name: 'CareTeam'}">
-                        <button id="myButton" class="btn">Continue</button>
-                      </router-link>
-                      <div class="groups">
-                        <span><i class="fa fa-file-text-o"></i> Feed</span>
-                      </div>
-                      <div>Feed</div>
-                      <div class="lodmore"><button class="btn-view btn-load-more"></button></div>
-                    </div><!-- photos -->
+                  <div class="central-meta new-pst item">
+                    <div class="new-postbox">
+                        <figure>
+                            <img src="../images/resources/admin2.jpg" alt="">
+                        </figure>
+                        <div class="newpst-input">
+                            <form method="post">
+                                <textarea rows="2" placeholder="Add new insight or note... "></textarea>
+                                <div class="attachments">
+                                    <ul>
+
+                                        <li>
+                                            <button type="submit">Add New Insight</button>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div><!-- add post new box -->
+
+                <div class="central-meta item">
+                    <div class="tab-content">
+                      <div class="tab-pane active fade show " id="frends" >
+                        <ul class="nearby-contct">
+                            <li>
+                                <div class="nearly-pepls">
+                                    <figure>
+                                        <a href="time-line.html" title=""><img src="../images/resources/friend-avatar.jpg" alt=""></a>
+                                    </figure>
+                                    <div class="pepl-info">
+                                        <h4><a href="time-line.html" title="">3 Month Overdue Checkup!</a></h4>
+                                        <span>Dr. Bucky Barnes</span>
+                                        <a href="time-line.html" title="" class="add-butn" data-ripple="">Schedule Appointment</a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                       </div>
+                    </div>
+                </div>
+
+
+                <div class="central-meta item">
+                        <div class="tab-content">
+                           <div class="tab-pane active fade show " id="frends" >
+                               <ul class="nearby-contct">
+                                    <li>
+                                        <div class="nearly-pepls">
+                                            <figure>
+                                                <a href="time-line.html" title=""><img src="../images/resources/friend-avatar3.jpg" alt=""></a>
+                                            </figure>
+                                            <div class="pepl-info">
+                                                <h4><a href="time-line.html" title="">1 Month Overdue Checkup! </a></h4>
+                                                <span>Dr. Jason Borne</span>
+                                                <a href="time-line.html" title="" class="add-butn" data-ripple="">Schedule Appointment</a>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                </ul>
+
+                           </div>
+
+                        </div>
+                </div>
+
+                <div class="central-meta item">
+                                    <div class="tab-content">
+                                      <div class="tab-pane active fade show " id="frends" >
+                                        <ul class="nearby-contct">
+                                            <li>
+                                                <div class="nearly-pepls">
+                                                  
+                                                    <div class="pepl-info">
+                                                        <h4><a href="time-line.html" title="">4 Month Overdue Glucose Check!</a></h4>
+
+                                                        <a href="time-line.html" title="" class="add-butn" data-ripple="">Connect Data</a>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                       </div>
+                                    </div>
+                                </div>
+                <div class="central-meta item">
+                        <div class="tab-content">
+                           <div class="tab-pane active fade show " id="frends" >
+                               <ul class="nearby-contct">
+                                    <li>
+                                        <div class="nearly-pepls">
+                                            <div class="pepl-info">
+                                                <h4><a href="time-line.html" title="">Congrats! Step Goal Met for 90 Days ! </a></h4>
+                                                <span>6000/day</span>
+                                            </div>
+                                        </div>
+                                    </li>
+
+                                </ul>
+
+                           </div>
+
+                        </div>
+                </div>
+                <div class="central-meta item">
+                                        <div class="tab-content">
+                                           <div class="tab-pane active fade show " id="frends" >
+                                               <ul class="nearby-contct">
+                                                    <li>
+                                                        <div class="nearly-pepls">
+                                                            <div class="pepl-info">
+                                                                <h4><a href="time-line.html" title="">You've been enrolled in a nutrition planning program! </a></h4>
+                                                                <span>Note from Dr. Cameron Diaz</span>
+                                                            </div>
+                                                        </div>
+                                                    </li>
+
+                                                </ul>
+
+                                           </div>
+
+                                        </div>
+                                </div>
+
+                <div class="central-meta item">
+                    <div class="user-post">
+                        <div class="friend-info">
+                            <figure>
+                                <img src="../images/resources/friend-avatar10.jpg" alt="">
+                            </figure>
+                            <div class="friend-name">
+                                <ins><a href="time-line.html" title="">Anon123</a></ins>
+                                <span>published: june,2 2018 19:PM</span>
+                            </div>
+                            <div class="description">
+
+                                    <p>
+                                        I've been tracking posible nutrition influences on my fatigue for 90 days and saw dramatic improvements when increases magnesium intakes, and decreases in fatigue when magnesium intake was low. Hope this helps someone. I'll post my data in the upcoming days.
+                                    </p>
+                                </div>
+                            <div class="post-meta">
+                                <div class="we-video-info">
+                                    <ul>
+
+                                        <li>
+                                            <span class="views" data-toggle="tooltip" title="views">
+
+                                                <p>1.2k views</p>
+                                            </span>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+
                   </div><!-- centerl meta -->
+
                   <div class="col-lg-3">
+
                     <div class="sidebar static">
                       <div class="widget">
                         <h4 class="widget-title">Your Health Page</h4>
                         <div class="your-page">
+                            <figure>
+                                <a title="" href="#"><img alt="" src="../images/resources/admin2.jpg"></a>
+                            </figure>
+                            <div class="page-meta">
+                                <a class="underline" title="" href="#">Maxine Johnson</a>
+                                <span><i class="ti-comment"></i>Messages <em>0</em></span>
+                                <span><i class="ti-bell"></i>Notifications <em>0</em></span>
+                            </div>
+
+                        </div>
+                        <h5 class="widget-title"><i class="ti-heart"></i>Tracking</h5>
+                        <div style="
+                                                            padding-left: 20px;
+                                                            padding-right: 20px;
+                                                        ">
+                        <p>Diabetes, Physical Activity, Diet, Glucose, Sleep, Fatigue</p>
+                           <a href="#" title="" class="underline" style="font-size: .8rem;">Add Items</a> <br>
+                           <a href="#" title="" class="underline" style="font-size: .8rem;">Edit Data Connections</a>
+                        </div>
+
+                        <div class="your-page">
                           <div>
                             <a title="" href="#"><img alt="" src=" ../images/resources/friend-avatar9.jpg"></a>
                           </div>
-                          <div class="page-meta">
-                            <a class="underline" title="" href="#">My page</a>
-                            <span><i class="ti-comment"></i>Messages <em>9</em></span>
-                            <span><i class="ti-bell"></i>Notifications <em>2</em></span>
-                          </div>
+
                           <div class="page-likes">
-                            <ul class="nav nav-tabs likes-btn">
-                              <li class="nav-item"><a data-toggle="tab" href="#link1" class="active">likes</a></li>
-                              <li class="nav-item"><a data-toggle="tab" href="#link2" class="">views</a></li>
-                            </ul>
+
                             <!-- Tab panes -->
-                            <div class="widget friend-list stick-widget">
-                              <h4 class="widget-title">Health Team</h4>
-                              <div id="searchDir"></div>
-                              <ul class="friendz-list">
-                              </ul>
-                            </div><!-- friends list sidebar -->
+                                    <div class="widget friend-list stick-widget">
+										<h4 class="widget-title">Care Team</h4>
+										<div id="searchDir"></div>
+										<ul id="people-list" class="friendz-list">
+											<li>
+												<figure>
+													<img src="../images/resources/friend-avatar.jpg" alt="">
+													<span class="status f-online"></span>
+												</figure>
+												<div class="friendz-meta">
+													<a href="time-line.html">Dr. bucky barnes</a>
+													<i>PCP</i>
+												</div>
+											</li>
+											<li>
+												<figure>
+													<img src="../images/resources/friend-avatar2.jpg" alt="">
+													<span class="status f-away"></span>
+												</figure>
+												<div class="friendz-meta">
+													<a href="time-line.html">Dr. Sarah Loren</a>
+													<i>Pharmacist</i>
+												</div>
+											</li>
+											<li>
+												<figure>
+													<img src="../images/resources/friend-avatar3.jpg" alt="">
+													<span class="status f-off"></span>
+												</figure>
+												<div class="friendz-meta">
+													<a href="time-line.html">Dr. jason borne</a>
+													<i>Endocrinologists</i>
+												</div>
+											</li>
+											<li>
+												<figure>
+													<img src="../images/resources/friend-avatar4.jpg" alt="">
+													<span class="status f-off"></span>
+												</figure>
+												<div class="friendz-meta">
+													<a href="time-line.html">Dr. Cameron diaz</a>
+													<i>Dietician</i>
+												</div>
+											</li>
+
+										</ul>
+									</div><!-- friends list sidebar -->
+
                           </div>
                         </div>
                       </div>
                     </div>
+                    <router-link :to="{name: 'CareTeam'}">
+                                        <button id="myButton" class="btn">Continue</button>
+                                      </router-link>
                   </div><!-- sidebar -->
+
                 </div>
               </div>
             </div>
           </div>
         </div>
       </section>
+
       <div class="bottombar">
         <div class="container">
           <div class="row">
